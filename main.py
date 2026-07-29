6b74701ac5e9aa0a4fedf19216f003099a2dcd73
Harishnankj <harikrishnankjharihkkj11@gmail.com>
2026-07-29 12:37:51 +0530

    fix: change volume syntax to '+200%' because '+15dB' is rejected by edge-tts

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def text_to_speech(text, output_path, lang='en'):
     voice = 'ml-IN-SobhanaNeural' if lang == 'ml' else 'en-US-JennyNeural'
 
     async def _generate():
-        communicate = edge_tts.Communicate(text, voice, volume="+15dB")
+        communicate = edge_tts.Communicate(text, voice, volume="+200%")
         await communicate.save(output_path)
 
     try:
